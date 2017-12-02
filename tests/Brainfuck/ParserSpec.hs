f79c68d2d520a0ab503d591b710b634418457bcb
maarten <maarten.j.jacobs@gmail.com>
2017-12-02 15:24:32 +0000

    Remove unnecessary parentheses

--- tests/Brainfuck/ParserSpec.hs
+++ tests/Brainfuck/ParserSpec.hs
@@ -7,25 +7,25 @@ spec :: Spec
 spec =
     describe "brainfuck parser" $ do
         it "parses increments and decrements" $ do
-            (parse "+") `shouldBe` (Right [Increment])
-            (parse " + ") `shouldBe` (Right [Increment])
-            (parse "-") `shouldBe` (Right [Decrement])
-            (parse " - ") `shouldBe` (Right [Decrement])
+            parse "+" `shouldBe` Right [Increment]
+            parse " + " `shouldBe` Right [Increment]
+            parse "-" `shouldBe` Right [Decrement]
+            parse " - " `shouldBe` Right [Decrement]
         it "parses movements" $ do
-            (parse "<") `shouldBe` (Right [MoveLeft])
-            (parse " < ") `shouldBe` (Right [MoveLeft])
-            (parse ">") `shouldBe` (Right [MoveRight])
-            (parse " > ") `shouldBe` (Right [MoveRight])
+            parse "<" `shouldBe` Right [MoveLeft]
+            parse " < " `shouldBe` Right [MoveLeft]
+            parse ">" `shouldBe` Right [MoveRight]
+            parse " > " `shouldBe` Right [MoveRight]
         it "parses I/O" $ do
-            (parse ".") `shouldBe` (Right [Output])
-            (parse " . ") `shouldBe` (Right [Output])
-            (parse ",") `shouldBe` (Right [Input])
-            (parse " , ") `shouldBe` (Right [Input])
+            parse "." `shouldBe` Right [Output]
+            parse " . " `shouldBe` Right [Output]
+            parse "," `shouldBe` Right [Input]
+            parse " , " `shouldBe` Right [Input]
         it "parses loops" $ do
-            (parse "[]")  `shouldBe` (Right [Loop []])
-            (parse "[+]")  `shouldBe` (Right [Loop [Increment]])
-            (parse "+[+]") `shouldBe` (Right [Increment, Loop [Increment]])
-            (parse "+[+].") `shouldBe` (Right [Increment, Loop [Increment], Output])
-            (parse " + [ + ] . ") `shouldBe` (Right [Increment, Loop [Increment], Output])
-            (parse " + [ + [ + ] + ] . ") `shouldBe`
-                (Right [Increment, Loop [Increment, Loop [Increment], Increment], Output])
+            parse "[]"  `shouldBe` Right [Loop []]
+            parse "[+]"  `shouldBe` Right [Loop [Increment]]
+            parse "+[+]" `shouldBe` Right [Increment, Loop [Increment]]
+            parse "+[+]." `shouldBe` Right [Increment, Loop [Increment], Output]
+            parse " + [ + ] . " `shouldBe` Right [Increment, Loop [Increment], Output]
+            parse " + [ + [ + ] + ] . " `shouldBe`
+                Right [Increment, Loop [Increment, Loop [Increment], Increment], Output]
